b945946ecd7b88e91e6eee0ddfde2a5a7f0f6c59
dropout <ienoneko2025@outlook.jp>
2025-12-01 13:19:01 +0900

    require python 3.12

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+from sys import exit, stderr, version_info
+
+if version_info < (3, 12):
+  print('Python too old', file=stderr)
+  exit(1)
+
 from PySide6.QtWidgets import QApplication
 
 from loader_dlg import LoaderDialog
